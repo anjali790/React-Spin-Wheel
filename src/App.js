6d9ce2f20755ea715e3a430f3cad32474d94cdbf
anjali790 <anjaliruby790@gmail.com>
2022-12-07 16:34:24 +0530

    spin wheel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,96 @@
-import './App.css';
-import { SpinWheel } from './components/SpinWheel';
+import { React, useState } from "react";
+import { Button, Modal, Container, Row, Col } from "react-bootstrap";
+
+import { SpinWheel } from "./components/SpinWheel";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
+  const [spinning, setSpinning] = useState(false);
+  const [winners, setWinners] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+
+  if (window.localStorage.getItem("duration") === null)
+    localStorage.setItem("duration", 10);
+
+  const [items, setItems] = useState(
+    [
+      "Flat 20% off",
+      "Blanc 50ml 499",
+      "Flat $50 off",
+      "EDT Noir $99",
+      "$500 Voucher",
+      "Free Gift Box",
+    ]
+  )
+
+  function cancelModal() {
+    setOpenModal(false);
+  }
+
+  function removeWinnerModal() {
+    const winner = winners[winners.length - 1];
+    const index = items.indexOf(winner);
+    items.splice(index, 1);
+    setItems(items);
+    setOpenModal(false);
+    console.log(`Removed ${winner} from entries.`);
+    localStorage.setItem("itemsList", JSON.stringify(items));
+  }
+
+  function selectResultEventHandler(data) {
+    if (items.length > 0 && spinning !== true) {
+      var selectedIndex = data;
+
+      setSpinning(true);
+
+      setTimeout(() => {
+        setSpinning(false);
+
+      }, window.localStorage.getItem("duration") * 1000);
+
+      setTimeout(() => {
+        setWinners(items[selectedIndex]);
+
+      }, window.localStorage.getItem("duration") * 1000);
+
+      setTimeout(() => {
+        setOpenModal(true);
+      }, window.localStorage.getItem("duration") * 1000);
+    }
+  }
+
   return (
-    <>
-      <SpinWheel />
-    </>
+    <div>
+      <Modal show={openModal} onHide={cancelModal} size="lg">
+        <Modal.Header closeButton className="container">
+          <Modal.Title>Congrats! You won: 🎉</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>{winners}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={cancelModal}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={removeWinnerModal}>
+            Remove
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Container fluid>
+        <Row>
+          <Col className="mt-4" lg="6" md="auto">
+            <SpinWheel
+              items={items}
+              onChange={selectResultEventHandler}
+              spinning={spinning}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
--- src/components/SpinWheel.jsx
+++ src/components/SpinWheel.jsx
@@ -1,67 +1,76 @@
-import React from 'react';
+import React from "react";
 
-import './spinwheel.css';
-
+import "./spinwheel.css";
 export class SpinWheel extends React.Component {
-    state = {
-        name: "circle"
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedItem: null,
+        };
+        this.selectItem = this.selectItem.bind(this);
     }
 
-    startRotate = () => {
-        this.setState({
-            name: "circle start-rotate"
-        });
-        setTimeout(() => {
-            this.setState({
-                name: "circle start-rotate stop-rotate"
-            });
-        }, Math.floor(Math.random() * 10000) + 1);
+    selectItem() {
+        if (this.state.selectedItem === null) {
+            const selectedItem = Math.floor(Math.random() * this.props.items.length);
+            this.props.onChange(selectedItem);
+            if (this.props.onSelectItem) {
+                this.props.onSelectItem(selectedItem);
+            }
+            this.setState({ selectedItem });
+        } else {
+            this.setState({ selectedItem: null });
+            setTimeout(this.selectItem, 500);
+        }
     }
 
     render() {
+        const { selectedItem } = this.state;
+        const { items } = this.props;
+
+        const wheelVars = {
+            "--nb-item": items.length,
+            "--selected-item": selectedItem,
+        };
+        const spinning = selectedItem !== null ? "spinning" : "";
+
+        let cssProperties = {};
+
+        cssProperties["--wheel-color"] = 'rgb(24, 37, 90)';
+
         return (
-            <div>
-                <div className='arrow'></div>
-                <ul className={this.state.name}>
-                    <li>
-                        <div className='text'>1</div>
-                    </li>
-                    <li>
-                        <div className='text'>2</div>
-                    </li>
-                    <li>
-                        <div className='text'>3</div>
-                    </li>
-                    <li>
-                        <div className='text'>4</div>
-                    </li>
-                    <li>
-                        <div className='text'>5</div>
-                    </li>
-                    <li>
-                        <div className='text'>6</div>
-                    </li>
-                    <li>
-                        <div className='text'>7</div>
-                    </li>
-                    <li>
-                        <div className='text'>8</div>
-                    </li>
-                    <li>
-                        <div className='text'>9</div>
-                    </li>
-                    <li>
-                        <div className='text'>10</div>
-                    </li>
-                    <li>
-                        <div className='text'>11</div>
-                    </li>
-                    <li>
-                        <div className='text'>12</div>
-                    </li>
-                </ul>
-                <button className='spin-btn' onClick={this.startRotate}>SPIN</button>
+            <div style={cssProperties}>
+                <h1 className="text">Spin the wheel for a Surprise offer</h1>
+                <div className="wheel-container">
+                    <div
+                        lg={true}
+                        className={`wheel ${spinning}`}
+                        style={wheelVars}
+                        onClick={this.selectItem}
+                    >
+                        {items.map((item, index) => (
+                            <div
+                                className="wheel-item"
+                                key={index}
+                                style={{ "--item-nb": index }}
+                            >
+                                {item}
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
-        )
+        );
     }
 }
+
+SpinWheel.defaultProps = {
+    items: [
+        "Flat 20% off",
+        "Blanc 50ml 499",
+        "Flat $50 off",
+        "EDT Noir $99",
+        "$500 Voucher",
+        "Free Gift Box",
+    ],
+};
